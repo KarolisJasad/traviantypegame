c27fdcde338c94be22f41a52e4f97d3840979820
Karolis Jasadavičius <karolisjasad@gmail.com>
2023-06-15 09:48:10 +0300

    More setup

--- travian_project/travian_project/settings.py
+++ travian_project/travian_project/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR.joinpath(STATIC_URL)
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR.joinpath(MEDIA_URL)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
